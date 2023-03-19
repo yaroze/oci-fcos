ae0cf2f9058b36e9f97249fa646b7bbc9595fc33
Pedro F <pedro@yaroze.org>
2023-03-19 17:48:23 +0000

    Adding a Terraform stub

---
+++ terraform/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+    }
+  }
+}
+
+provider "oci" {
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  private_key_path = var.private_key_path
+  region = var.region
+}
+
+resource "oci_core_instance" "example" {
+  availability_domain = var.availability_domain
+  compartment_id = var.compartment_id
+  display_name = "example-instance"
+  shape = "VM.Standard.A1.Flex"
+
+  shape_config {
+    ocpus = 1
+    memory_in_gbs = 1
+  }
+
+  metadata = {
+    ssh_authorized_keys = "ssh-rsa ***REMOVED***"
+  }
+
+  source_details {
+    source_type = "image"
+    source_id = var.image_id
+  }
+
+  create_vnic_details {
+    subnet_id = var.subnet_id
+  }
+
+  timeouts {
+    create = "30m"
+    delete = "30m"
+  }
+
+  launch_options {
+    firmware = "UEFI_64"
+    network_type = "PARAVIRTUALIZED"
+  }
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "tenancy_ocid" {
+  type = string
+}
+
+variable "user_ocid" {
+  type = string
+}
+
+variable "fingerprint" {
+  type = string
+}
+
+variable "private_key_path" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "availability_domain" {
+  type = string
+}
+
+variable "compartment_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "image_id" {
+  type = string
+}
