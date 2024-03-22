243563e9f2b6b5b661e109d0b5ce5cd9f132399e
Dylan M. Taylor <dylan@dylanmtaylor.com>
2024-03-21 20:06:36 -0400

    Switch script to podman, run for both aarch64 and x86_64

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 check_status(){
         if [ $? -eq 0 ]
     then 
@@ -12,62 +11,66 @@ check_status(){
     fi
 }
 
+
 get_sha256sum(){
-    SHA=($(sha256sum $1))
+    SHA=($(sha256sum "$1"))
 }
 
 echo -n "Generating ignition file out of butane..."
-docker  run -i  --rm quay.io/coreos/butane:release --pretty --strict < butane.bu > config.ign
+podman run -i --rm quay.io/coreos/butane:release --pretty --strict < butane.bu > config.ign
 check_status
 
+ARCHITECTURES=("aarch64" "x86_64")
 
-export URL=`curl -s https://builds.coreos.fedoraproject.org/streams/stable.json | jq -r '.architectures.aarch64.artifacts.qemu.formats."qcow2.xz".disk.location' `
-export SHA256=`curl -s https://builds.coreos.fedoraproject.org/streams/stable.json | jq -r '.architectures.aarch64.artifacts.qemu.formats."qcow2.xz".disk."sha256"'`
-export SHA256_UNCOMPRESSED=`curl -s https://builds.coreos.fedoraproject.org/streams/stable.json | jq -r '.architectures.aarch64.artifacts.qemu.formats."qcow2.xz".disk."uncompressed-sha256"'`
+for ARCH_TYPE in "${ARCHITECTURES[@]}"; do
+    echo "Checking if we already have the necessary images for $ARCH_TYPE..."
 
-echo "Checking if we already have the necessary images..."
+    export URL=$(curl -s "https://builds.coreos.fedoraproject.org/streams/stable.json" | jq -r ".architectures.$ARCH_TYPE.artifacts.qemu.formats.\"qcow2.xz\".disk.location")
+    export SHA256=$(curl -s "https://builds.coreos.fedoraproject.org/streams/stable.json" | jq -r ".architectures.$ARCH_TYPE.artifacts.qemu.formats.\"qcow2.xz\".disk.\"sha256\"")
+    export SHA256_UNCOMPRESSED=$(curl -s "https://builds.coreos.fedoraproject.org/streams/stable.json" | jq -r ".architectures.$ARCH_TYPE.artifacts.qemu.formats.\"qcow2.xz\".disk.\"uncompressed-sha256\"")
 
-if [ ! -f fcos.qcow2 ]; then 
-    echo "Uncompressed image doesn't exist. Checking if we have a compressed image here... 🤔
-"
+    if [ ! -f fcos_$ARCH_TYPE.qcow2 ]; then 
+        echo "Uncompressed image doesn't exist. Checking if we have a compressed image here... 🤔"
 
-    if [ ! -f fcos.qcow2.xz ]; then
-        echo -n "Compressed image doesn't exist either. Downloading... ⏳"
-        curl -s $URL -o fcos.qcow2.xz
-        check_status
-        echo -n "Decompressing... ⏳"
-        xz -k -d fcos.qcow2.xz
-        check_status
-    else
-        echo -n "Checking sha256sum for compressed image... ⏳"
-        get_sha256sum fcos.qcow2.xz
-        if [ "$SHA" != "$SHA256_COMPRESSED" ]; then
-            false
+        if [ ! -f fcos_$ARCH_TYPE.qcow2.xz ]; then
+            echo -n "Compressed image doesn't exist either. Downloading... ⏳"
+            # Fetch image URL and SHA256 sum based on architecture
+            curl -s "$URL" -o fcos_$ARCH_TYPE.qcow2.xz
             check_status
-
+            echo -n "Decompressing... ⏳"
+            xz -k -d fcos_$ARCH_TYPE.qcow2.xz
+            check_status
+        else
+            echo -n "Checking sha256sum for compressed image... ⏳"
+            get_sha256sum fcos_$ARCH_TYPE.qcow2.xz
+            if [ "${SHA[0]}" != "$SHA256_COMPRESSED" ]; then
+                echo "known good: $SHA256_COMPRESSED"
+                echo "calculated: ${SHA[0]}"
+                false
+                check_status
+            fi
         fi
     fi
-fi
 
+    echo "Comparing sha256sum of decompressed image... ⏳"
+    get_sha256sum fcos_$ARCH_TYPE.qcow2
+    if [ "${SHA[0]}" != "$SHA256_UNCOMPRESSED" ]; then 
+        echo -n "sha256sum mismatch! "
+        echo "known good: $SHA256_UNCOMPRESSED"
+        echo "calculated: ${SHA[0]}"
+        false
+        check_status
+        exit 1
+    else
+        true
+        check_status
+    fi
+done
 
-echo "Comparing sha256sum of decompressed image... ⏳"
-get_sha256sum fcos.qcow2
-if [ "$SHA" != "$SHA256_UNCOMPRESSED" ]; then 
-    echo -n "
-sha256sum mismatch! "
-    false
-    check_status
-    exit 1
-else
-    true
-    check_status
-fi
-
-
-echo "Building Docker image... if the image isn't build already or if guestfish.sh isn't updated, this might take a while... 🐳"
+echo "Building Docker image... if the image isn't built already or if guestfish.sh isn't updated, this might take a while... 🐳"
 echo -n "Logs are under $PWD/build_guestfish.log..."
-docker build . -t guestfish > build_guestfish.log 2>&1
+podman build . -t guestfish > build_guestfish.log 2>&1
 check_status
 
-echo "Launching docker container to inject ignition file... ⏳"
-docker run -v$PWD:/root:z -w /root/fcos guestfish /root/guestfish.sh
+echo "Launching podman container to inject ignition file... ⏳"
+podman run -v "$PWD:/root:z" -w /root/fcos guestfish /root/guestfish.sh
